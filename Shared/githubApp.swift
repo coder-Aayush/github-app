3a0845998aaf707bab4eb3c17020784e43068081
coder-Aayush <bhattaraiaayush08@gmail.com>
2022-07-25 13:09:54 +0545

    Initial Commit

---
+++ Shared/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  Shared
+//
+//  Created by Fatdog on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
